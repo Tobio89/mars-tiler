77b88ea3d4186e2f470112465ead93f54ac6de9b
Tobias S <tobio@lunit.io>
2025-10-23 18:16:51 +0900

    feat: add files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { AppService } from './app.service';
 import { TilesController } from './tiles/tiles.controller';
 import { TilesService } from './tiles/tiles.service';
 import { ConfigModule } from '@nestjs/config';
+import { DataController } from './data/data.controller';
+import { DataService } from './data/data.service';
+import { ConfigController } from './config/config.controller';
+import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, TilesController],
-  providers: [AppService, TilesService],
+  controllers: [AppController, TilesController, DataController, ConfigController],
+  providers: [AppService, TilesService, DataService, ConfigService],
 })
 export class AppModule {}
