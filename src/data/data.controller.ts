30580691674173a246824bc11c2e3730e993be12
Tobias S <tobio@lunit.io>
2025-10-23 18:36:43 +0900

    feat: add data route

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import marsLocaleData from '../../assets/data/marsLocaleData.json';
 
 @Controller('data')
-export class DataController {}
+export class DataController {
+  @Get(':data_category')
+  getLocaleData(@Param('data_category') data_category: string): object {
+    if (data_category === 'mountains') {
+      return marsLocaleData['mountains'];
+    }
+
+    if (data_category === 'missionSites') {
+      return marsLocaleData['missionSites'];
+    }
+    throw new NotFoundException('Invalid data category');
+  }
+}
