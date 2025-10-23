2f9fd57d62a14a01a0fe06a3cf3bd16736e33847
Tobias S <tobio@lunit.io>
2025-10-23 18:38:38 +0900

    feat: add config route

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,16 @@ import { TilesController } from './tiles/tiles.controller';
 import { TilesService } from './tiles/tiles.service';
 import { ConfigModule } from '@nestjs/config';
 import { DataController } from './data/data.controller';
-import { DataService } from './data/data.service';
 import { ConfigController } from './config/config.controller';
-import { ConfigService } from './config/config.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, TilesController, DataController, ConfigController],
-  providers: [AppService, TilesService, DataService, ConfigService],
+  controllers: [
+    AppController,
+    TilesController,
+    DataController,
+    ConfigController,
+  ],
+  providers: [AppService, TilesService],
 })
 export class AppModule {}
--- src/config/config.controller.ts
+++ src/config/config.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import visConfig from '../../assets/config/visualizationConfig.json';
 
 @Controller('config')
-export class ConfigController {}
+export class ConfigController {
+  @Get()
+  getLocaleData(): object {
+    return visConfig;
+  }
+}
